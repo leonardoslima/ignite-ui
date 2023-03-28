65474f7ec3123c51a3093059acfbbd45bb6c2547
Leonardo <leonardosilvalima93@live.com>
2023-03-28 14:59:09 -0300

    fix add subpath to Storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/estudos-ignite/'
+      config.base = '/ignite-ui/'
     }
 
     return config
